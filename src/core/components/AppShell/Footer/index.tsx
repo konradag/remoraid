39c32c293b6be3f06f523e7f9841d5298b29f188
Konrad Goldammer <konradgoldammer@gmail.com>
2025-06-10 16:57:54 +0200

    [core] Update color of icon in 'Footer'

--- src/core/components/AppShell/Footer/index.tsx
+++ src/core/components/AppShell/Footer/index.tsx
@@ -1,19 +1,11 @@
-import { Group, useMantineTheme } from "@mantine/core";
+import { Group } from "@mantine/core";
 import { IconPennant } from "@tabler/icons-react";
 import { ReactNode } from "react";
-import { useHydratedMantineColorScheme } from "../../RemoraidProvider/HydrationStatusProvider";
 
 export default function Footer(): ReactNode {
-  const theme = useMantineTheme();
-  const { colorScheme } = useHydratedMantineColorScheme();
   return (
     <Group justify="center" w="100%" py="md">
-      <IconPennant
-        size={50}
-        color={
-          colorScheme === "dark" ? theme.colors.dark[8] : theme.colors.gray[3]
-        }
-      />
+      <IconPennant size={50} color="var(--mantine-color-default-border)" />
     </Group>
   );
 }
