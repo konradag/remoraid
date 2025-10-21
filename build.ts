764075fb81f165a6736eee155239e2f8910da2ea
Konrad Goldammer <konradgoldammer@gmail.com>
2025-10-21 22:32:48 +0200

    Fix 'jsxDEV' crash during prerender in build script

--- build.ts
+++ build.ts
@@ -11,6 +11,10 @@ const sharedBuildConfig: (
   withClientBanner?: boolean
 ) => Omit<BuildConfig, "entrypoints"> = (p, withClientBanner) => ({
   outdir: `./dist/${p ?? ""}`,
+  jsx: "automatic",
+  define: {
+    "process.env.NODE_ENV": '"production"',
+  },
   external: [
     ...Object.keys(packageJson.peerDependencies || {}),
     ...packages.filter((e) => e !== p).map((e) => `remoraid/${e}`),
